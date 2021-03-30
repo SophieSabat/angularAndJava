d8381e6b357ea9c5c3a24338815d3f65077edc23
SophieSabat <sonyasab2000@gmail.com>
2021-03-30 20:34:57 +0300

    Angular for Java

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angJavaTask';
+export class AppComponent implements OnInit{
+  car = {model: '', id: ''
+  };
+  cars: any[];
+  constructor(private httpClient: HttpClient) {
+  }
+  // tslint:disable-next-line:variable-name
+  private _url = 'http://localhost:8080/cars';
+
+  save(myForm: any): void {
+    console.log(this.car);
+    this.httpClient.post<any>(this._url + '/save', this.car)
+      .subscribe(value => console.log(value));
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<any[]>(this._url)
+      .subscribe(value => this.cars = value);
+  }
+
+  delete(id): void {
+    console.log(this.car);
+    this.httpClient.delete<any>(this._url + '/delete/' + id)
+      .subscribe(value => console.log(value));
+  }
 }
